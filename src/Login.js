d65db7ff76adce35513b72e5f2051711bee29d18
Geobilly <102395479+Geobilly@users.noreply.github.com>
2023-11-27 13:58:13 +0000

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -13,7 +13,7 @@ const Login = ({ onLogin }) => {
   const handleLogin = async () => {
   try {
     setLoading(true);
-    const response = await axios.post('https://kempstasksystem.onrender.com/login', {
+    const response = await axios.post('https://sqlreportsystem.onrender.com/login', {
       username,
       password,
     });
