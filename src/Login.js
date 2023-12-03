07df8cd610a82fd911d5587831fff389f9f44ff7
George Abban <102395479+Geobilly@users.noreply.github.com>
2023-12-03 00:09:08 +0000

    Update 3 files

--- src/Login.js
+++ src/Login.js
@@ -138,14 +138,14 @@ const Login = ({ onLogin }) => {
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
+                {/* <Link href="#" variant="body2">
+                  Forgot password
+                </Link> */}
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                {/* <Link href="#" variant="body2">
                   {"Don't have an account? Sign Up"}
-                </Link>
+                </Link> */}
               </Grid>
             </Grid>
           </Box>
